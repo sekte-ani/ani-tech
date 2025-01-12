403d21f6f27f03107aae475686fa07a7199192a0
Gswaraw37 <genta.swara@gmail.com>
2025-01-13 00:13:50 +0700

    fix interface

--- resources/views/landingPage/index.blade.php
+++ resources/views/landingPage/index.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/styles.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <title>@yield('title')</title>
 </head>
@@ -17,14 +17,14 @@
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="{{ asset('images/logo.png') }}" class="h-16" alt="Example Image">
+                    <img src="{{ secure_asset('images/logo.png') }}" class="h-16" alt="Example Image">
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ANI</span>
                 </a>
                 <button data-collapse-toggle="navbar-default" type="button"
                     class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     aria-controls="navbar-default" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="https://www.w3.org/2000/svg" fill="none"
                         viewBox="0 0 17 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M1 1h15M1 7h15M1 13h15" />
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     server: {
-        https: true, // Mengaktifkan HTTPS untuk server Vite
-        proxy: {
-            // Anda juga bisa menambahkan pengaturan proxy jika diperlukan
-            "/": "https://localhost", // Misalnya, proxy ke server lokal
-        },
+        https: true,
     },
     plugins: [
         laravel({
